d25ecb8b47f4d720daac1c83be5218dbe5309c2d
Juan Cruz Od <johnmanito3@gmail.com>
2023-10-11 05:01:07 +0200

    inicioPblicacion1

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Repasador en cuba</h1>
+      <h1>Repasador de Matematica</h1>
+      <h2>en Cuba</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
