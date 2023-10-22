6b714279a5dc2af873f4c9d235871b40fe9f822c
Juan Cruz Od <johnmanito3@gmail.com>
2023-10-23 01:28:45 +0200

    cursos1

--- src/Cursos.tsx
+++ src/Cursos.tsx
@@ -1,4 +1,7 @@
 import Nav from "./Nav";
+import  "./index.css"
+// import React from 'react'
+import ReactPlayer from 'react-player'
 
 export default function Cursos () {
 
@@ -8,11 +11,23 @@ return (
 
 <Nav/>
 
-<div className="ratio1 ratio-4x3">
-<div className="ratio2 ratio-4x3">
-  <iframe className="ratio ratio-4x3" src="https://www.youtube.com/embed/UFaMBm8-7mQ?rel=0" title="YouTube video" allowFullScreen/>
+
+<div className="ratio1">
+  <iframe className="ratio" src="https://www.youtube.com/embed/UFaMBm8-7mQ?rel=0" title="YouTube video" allowFullScreen />
 </div>
-</div>
+
+
+<div className='player-wrapper'>
+        <ReactPlayer
+          className='react-player'
+          url='https://www.youtube.com/watch?v=ugp2hjSycmk'
+          width='100%'
+          height='100%'
+          controls
+          loop
+          volume={1}
+        />
+      </div>
 
 </>
 
